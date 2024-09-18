3dffb2ecb828f53c39d0188b2c931f7de00d761a
janav-007 <janavdua27@gmail.com>
2024-09-18 13:48:20 +0530

    New DB Setup

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
  schema: "./utils/schema.ts",
   dialect: 'postgresql',
   dbCredentials: {
-    url: "postgresql://neondb_owner:SJvw4m2MVLyQ@ep-cold-mountain-a589xo3t-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
+    url: "postgresql://neondb_owner:dnsLyDmR2zj9@ep-super-star-a5vj7q3c.us-east-2.aws.neon.tech/neondb?sslmode=require"
   },
   verbose: true,
   strict: true,
