9a74d860fb53fb4acc91a06025a3d79c629b0bf4
janav-007 <janavdua27@gmail.com>
2024-11-04 13:15:19 +0530

    Production Deploy Build

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -18,8 +18,8 @@ function Footer() {
             <h1 className='font-semibold text-blue-400'>Prep<span className='text-white font-bold'>AI</span></h1>
         </div>
         <div className='flex flex-col text-right'>
-            <h3 className='text-gray-200 text-[12px] mr-2 font-medium'>Built on Next JS :)</h3>
-            <p className='text-gray-300 text-[8px] mr-2'>Made by @janavdua7</p>
+            <h3 className='text-gray-200 text-[12px] mr-2 font-medium'>PrepAI - SmartHire: Suite</h3>
+            <p className='text-gray-300 text-[8px] mr-2'>©️ All Rights Reserved</p>
         </div>
     </div>
   );
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
  schema: "./utils/schema.ts",
   dialect: 'postgresql',
   dbCredentials: {
-    url: "postgresql://neondb_owner:dnsLyDmR2zj9@ep-super-star-a5vj7q3c.us-east-2.aws.neon.tech/neondb?sslmode=require"
+    url: "postgresql://neondb_owner:aUwtGkmAl0s8@ep-crimson-waterfall-a5wp12et-pooler.us-east-2.aws.neon.tech/neondb?sslmode=require"
   },
   verbose: true,
   strict: true,
